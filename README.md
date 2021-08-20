00317176b217a5d9a804ab741886c68568aa1775
Micheal Jones <micheal@mcjones.ca>
2021-08-19 18:19:04 -0600

    Initial plotly implementation

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 Live Version: https://share.streamlit.io/chealion/streamlit-sunalta/main/poc.py
 
+## Data Sources
+
+Data Sources:
+
+Land Use: https://data.calgary.ca/dataset/Land-Use-Redesignation-Applications/33vi-ew4s
+
+Development: https://data.calgary.ca/dataset/Development-Permits/6933-unw5
+
+Building Permits: https://data.calgary.ca/Business-and-Economic-Activity/Building-Permits/c2es-76ed
+
+Tenancy Change: https://data.calgary.ca/dataset/Tenancy-Change-Applications/wrtt-2nqs
+
 ## Development
 
     streamlit run ./poc.py
--- poc.py
+++ poc.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pydeck as pdk
+import plotly.graph_objects as pgo
 
 from sodapy import Socrata
 
@@ -18,14 +18,12 @@ COMMUNITY_NAME = 'SUNALTA'
 DATE = '2020-12-31T00:00:00'
 
 # Set view
-st.set_page_config(page_title="Sunalta Development Map", layout="wide")
-
+st.set_page_config(page_title="Calgary Communities Development Map", layout="wide")
 
 # st.cache - will cache the output of the function
 # Keep cache for 24 hours - it's updated daily
 @st.cache(suppress_st_warning=True, ttl=60*60*24)
 def load_data(dataID):
-    # st.write("Cache miss: Loading data from data.calgary.ca")
     results = socrata_client.get(dataID,
                                  limit=100,
                                  communityname=COMMUNITY_NAME,
@@ -35,13 +33,36 @@ def load_data(dataID):
     data = pd.DataFrame.from_dict(results)
     return data
 
+# Separate loading for Land Use Data because it's not consistent with every other dataset
+@st.cache(suppress_st_warning=True, ttl=60*60*24)
+def load_land_use_data(dataID):
+    results = socrata_client.get(dataID,
+                                 limit=1000,
+                                 where="applieddate > '" + DATE + "'",
+                                 order="applieddate DESC",
+                                 exclude_system_fields=True)
+    data = pd.DataFrame.from_dict(results)
+    return data
 
 socrata_client = Socrata("data.calgary.ca", st.secrets["socrata_token"])
+
+# Load Land Use Data
+land_use_data = load_land_use_data(LAND_USE_ID)
+
+# Clean data for joint data frame display
+land_use_data = land_use_data.drop(['permittype',
+                                    'fromlud',
+                                    'proposedlud',
+                                    'locationcount',
+                                    'multipoint',
+                                    'completeddate'], 
+                                    axis=1)
+land_use_data = land_use_data.astype({"longitude": np.float64, "latitude": np.float64})
 
 # Load DP data
 dev_data = load_data(DEVELOPMENT_PERMIT_ID)
 
-# Clean data
+# Clean data for joint data frame display
 dev_data = dev_data.drop(['point',
                           'proposedusecode',
                           'communitycode',
@@ -59,7 +80,7 @@ dev_data = dev_data.astype({"longitude": np.float64, "latitude": np.float64})
 # Load BP data
 bp_data = load_data(BUILDING_PERMIT_ID)
 
-# Clean data
+# Clean data for joint data frame display
 bp_data = bp_data.drop(['permittypemapped',
                         'permitclassgroup',
                         'permitclassmapped',
@@ -80,62 +101,108 @@ bp_data = bp_data.astype({"longitude": np.float64, "latitude": np.float64})
 # Load tenancy data
 tc_data = load_data(TENANCY_CHANGE_ID)
 
-# Clean data
+# Clean data for joint data frame display
 tc_data = tc_data.drop(['permittype', 'communitycode', 'communityname', 'quadrant', 'ward', 'point'],  axis=1)
 tc_data = tc_data.astype({"longitude": np.float64, "latitude": np.float64})
 
-st.title("Sunalta Map")
-st.write("""Orange = Development Permits  
-Purple = Building Permits  
-Blue = Business Tenancy Changes""")
 
-st.pydeck_chart(pdk.Deck(
-    map_style='mapbox://styles/mapbox/light-v9',
-    tooltip=True,
-    initial_view_state=pdk.ViewState(
-         latitude=51.0415,
-         longitude=-114.1,
-         zoom=14,
-         pitch=0,
-     ),
-    layers=[
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=dev_data[['latitude', 'longitude']],
-            auto_highlight=True,
-            pickable=True,
-            get_position=["longitude", "latitude"],
-            get_radius=15,
-            radius_scale=1,
-            get_fill_color=[245, 121, 58, 255],
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=bp_data[['latitude', 'longitude']],
-            auto_highlight=True,
-            pickable=True,
-            get_position=["longitude", "latitude"],
-            get_radius=15,
-            radius_scale=1,
-            get_fill_color=[169, 90, 161, 255]
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=tc_data[['latitude', 'longitude']],
-            auto_highlight=True,
-            pickable=True,
-            get_position=["longitude", "latitude"],
-            get_radius=15,
-            radius_scale=1,
-            get_fill_color=[133, 192, 249, 255],
-        )
-    ],
+# Now we show off the map.
+st.title("Sunalta Map")
+
+fig = pgo.Figure()
+
+# Plotly's version of Layer is Trace
+# Zesty Colour Palette from https://venngage.com/blog/color-blind-friendly-palette/ 
+fig.add_trace(pgo.Scattermapbox(
+    lat=land_use_data['latitude'],
+    lon=land_use_data['longitude'],
+    mode='markers',
+    marker=pgo.scattermapbox.Marker(
+        size=13,
+        color='rgb(245,121,58)',
+        opacity=0.7
+    ),
+    text=land_use_data['permitnum'],
+    meta=land_use_data['statuscurrent'],
+    customdata=land_use_data['description'],
+    hovertemplate = "%{text}:<br>Status: %{meta}<br><br>Description: %{customdata}",
+    name='Land Use'
 ))
+
+fig.add_trace(pgo.Scattermapbox(
+    lat=dev_data['latitude'],
+    lon=dev_data['longitude'],
+    mode='markers',
+    marker=pgo.scattermapbox.Marker(
+        size=13,
+        color='rgb(169, 90, 161)',
+        opacity=0.7
+    ),
+    text=dev_data['permitnum'],
+    meta=dev_data['statuscurrent'],
+    customdata=dev_data['description'],
+    hovertemplate = "%{text}:<br>Status: %{meta}<br><br>Description: %{customdata}",
+    name='Development Permits'
+))
+
+fig.add_trace(pgo.Scattermapbox(
+    lat=bp_data['latitude'],
+    lon=bp_data['longitude'],
+    mode='markers',
+    marker=pgo.scattermapbox.Marker(
+        size=13,
+        color='rgb(133, 192, 249)',
+        opacity=0.7
+    ),
+    text=bp_data['permitnum'],
+    meta=bp_data['statuscurrent'],
+    customdata=bp_data['description'],
+    hovertemplate = "%{text}:<br>Status: %{meta}<br><br>Description: %{customdata}",
+    name='Building Permits'
+))
+
+fig.add_trace(pgo.Scattermapbox(
+    lat=tc_data['latitude'],
+    lon=tc_data['longitude'],
+    mode='markers',
+    marker=pgo.scattermapbox.Marker(
+        size=13,
+        color='rgb(15, 32, 128)',
+        opacity=0.7
+    ),
+    text=tc_data['permitnum'],
+    meta=tc_data['statuscurrent'],
+    customdata=tc_data['applicantname'],
+    hovertemplate = "%{text}:<br>Status: %{meta}<br><br>Description: %{customdata}",
+    name='Tenancy Changes'
+))
+
+fig.update_layout(
+    margin_pad=0,
+    height=650,
+    hovermode='closest',
+    clickmode='select',
+    showlegend=True,
+    mapbox=dict(
+        accesstoken=st.secrets['mapbox']['token'],
+        bearing=0,
+        center=dict(
+            lat=51.0425,
+            lon=-114.1
+        ),
+        pitch=0,
+        zoom=14,
+        style='light'
+    ),
+)
+
+st.plotly_chart(fig, use_container_width=True)
 
 # Rename some columns for nomalization
 bp_data = bp_data.rename(columns={"originaladdress": "address"})
 tc_data = tc_data.rename(columns={"applicantname": "applicant", "originaladdress": "address", "proposeduse": "description"})
 
+#all_data = pd.concat([dev_data, bp_data, tc_data, land_use_data])
 all_data = pd.concat([dev_data, bp_data, tc_data])
 all_data = all_data[['permitnum',
                      'address',
@@ -150,8 +217,10 @@ all_data = all_data[['permitnum',
 all_data.set_index('permitnum', inplace=True)
 all_data.sort_values(by=['applieddate'], ascending=False, inplace=True)
 
-st.table(all_data)
 
+# How to make these tables more useful. st.table sucks.
+st.table(all_data)
+st.table(land_use_data)
 st.markdown("""
 ----
 
